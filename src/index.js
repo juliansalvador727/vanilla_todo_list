3e3064bcc1894a0dadfab507285076fcad347da7
juliansalvador727 <juliansalvador727@gmail.com>
2025-07-26 00:33:31 -0700

    template html and css, added boiler code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+// index.js
 import "./styles/styles.css";
 import createItem from "./modules/toDoItem";
+import createProject from "./modules/toDoProject";
 
 console.log("hello list");
 const newItem = createItem({
@@ -12,3 +14,9 @@ const newItem = createItem({
 });
 
 console.log(newItem.getInfo());
+
+const newProject = createProject({ title: "test-project" });
+
+newProject.addItem(newItem);
+
+console.log(newProject.getProjectInfo());
