6e5c5ad9a260fe4d23e655b058f68d6bea32aedc
juliansalvador727 <juliansalvador727@gmail.com>
2025-07-26 23:11:30 -0700

    feat: delete projects

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@
 import "./styles/styles.css";
 import createItem from "./modules/toDoItem";
 import createProject from "./modules/toDoProject";
+import displayItems from "./modules/ui";
 
 console.log("hello list");
 const newItem = createItem({
-  title: "fortnite",
-  description: "awesome",
-  dueDate: "tomorrow",
+  title: "This is the title",
+  description: "This is the description",
+  dueDate: "This is the duedate",
   priority: "high",
   notes: "okay so notes",
   checked: true,
@@ -20,3 +21,5 @@ const newProject = createProject({ title: "test-project" });
 newProject.addItem(newItem);
 
 console.log(newProject.getProjectInfo());
+
+displayItems(newProject);
---
+++ src/modules/ui.js
@@ -0,0 +1,44 @@
+// ui.js
+
+const displayItems = (project) => {
+  const container = document.querySelector("#project-preview");
+  container.innerHTML = "";
+
+  //   const info = project.getProjectInfo();
+  //   const items = info.items;
+  const { items } = project.getProjectInfo();
+
+  items.forEach((item, index) => {
+    const { title, dueDate, checked } = item.getInfo();
+
+    const itemDiv = document.createElement("div");
+    itemDiv.classList.add("item");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = checked;
+
+    const titleElem = document.createElement("span");
+    titleElem.textContent = title;
+
+    const dueDateElem = document.createElement("span");
+    dueDateElem.textContent = dueDate;
+
+    const removeElem = document.createElement("button");
+    removeElem.textContent = `Remove?`;
+    removeElem.id = "remove-button";
+    removeElem.addEventListener("click", (e) => {
+      project.removeItem(index);
+      displayItems(project);
+    });
+
+    itemDiv.appendChild(titleElem);
+    itemDiv.appendChild(dueDateElem);
+    itemDiv.appendChild(checkbox);
+    itemDiv.appendChild(removeElem);
+
+    container.appendChild(itemDiv);
+  });
+};
+
+export default displayItems;
